b5163a0f1fd2d2831f35ef439a9c29c788e11284
Fidah <69793989+fidahhha@users.noreply.github.com>
2023-04-09 17:39:22 +1200

    Display information
    Shows the information of the option clicked from the drop down inside a widget

--- COMPX322/Assignment-2/AJAX.js
+++ COMPX322/Assignment-2/AJAX.js
@@ -1,79 +1,95 @@
-// AJAX GET request to commodity databse
-var request = new XMLHttpRequest();
-let getDataAjax = () => {
-  url = "AccessCommodities.php";
-  request.onload = displayData;
-  request.open("GET", url);
-  request.send("");
-};
-
-let displayData = () => {
-  var response = request.responseText;
-  theDivElement = document.getElementById("data");
-  theDivElement.innerHTML = response;
-};
-
-// Storying data into an 'Array of Object Literals'
-let commodityData = function () {
-  var commodity = {
-    id: [1, 2, 3, 4, 5, 6, 7],
-    name: ["Corn", "Cotton", "Sugar", "Copper", "Aluminium", "Wheat", "Coffee"],
-    information: [
-      "Global price of Corn",
-      "Global price of Cotton",
-      "Global price of Sugar, No. 11, World",
-      "Global price of Copper",
-      "Global price of Aluminium",
-      "Global price of Wheat",
-      "Global price of Coffee, Other Mild Arabica",
-    ],
-    code: ["CORN", "COTTON", "SUGAR", "COPPER", "ALUMINIUM", "WHEAT", "COFFEE"],
-  };
-
-  commodity.name = commodity.name.sort(function (a, b) {
-    var nameA = a.toUpperCase();
-    var nameB = b.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  // Populate commodity drop-down
-  var dropDown = document.getElementById("selectCommodity");
-  for (var i = 0; i < commodity.name.length; i++) {
-    var option = document.createElement("option");
-    option.text = commodity.name[i];
-    dropDown.add(option);
-  }
-  return commodity;
-};
-
-window.onload = function () {
-  commodityData();
-};
-
-// Creating commodities widget
-function commoditiesWidget(page_element, initial_data) {
-  var _data = new Array();
-  // anything we append to container will be displayed on the page
-  var _container = page_element;
-
-  var deleteButton = function (label, index, click_action) {
-    var _dom_element = document.createElement("input");
-    _dom_element.type = "button";
-    _dom_element.value = label;
-
-    _dom_element.onclick = function () {
-      click_action.call(null, index);
-    };
-
-    // Put this on the page
-    this.getDomElement = function () {
-      return _dom_element;
-    };
-  };
+// Define the commodity object
+function Commodity(id, name, information, code) {
+  this.id = id;
+  this.name = name;
+  this.information = information;
+  this.code = code;
 }
+
+// Create an array to store the commodities
+var commodityList = [];
+var widgets = [];
+var tempname;
+
+// Fetch the commodities data and build the form
+function setUpForm() {
+  fetch("AccessCommodities.php")
+    .then((response) => response.json())
+    .then(buildForm)
+    .catch((error) => console.error(error));
+}
+
+// Function to sort commodities by name
+function sortByName(a, b) {
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+}
+
+// Build the form options from the fetched commodities data
+function buildForm(options) {
+  const optionsSelect = document.getElementById("options-select");
+  for (let i = 0; i < options.length; i++) {
+    let c = new Commodity(
+      options[i].id,
+      options[i].name,
+      options[i].information,
+      options[i].code
+    );
+    commodityList.push(c);
+  }
+  commodityList.sort(sortByName);
+
+  // Populate drop down list
+  for (let j = 0; j < commodityList.length; j++) {
+    const option = document.createElement("option");
+    option.setAttribute("id", commodityList[j].id);
+    option.text = commodityList[j].name;
+    option.value = commodityList[j].id;
+    optionsSelect.appendChild(option);
+  }
+
+  // Checking if widget exists or not
+  optionsSelect.addEventListener("change", function () {
+    const selected = optionsSelect.options[optionsSelect.selectedIndex];
+    const selectedOption = selected.value;
+
+    if (widgets.includes(selectedOption)) {
+      return;
+    }
+    widgets.push(selectedOption);
+    const widget = makeWidget(selectedOption);
+    document.getElementById("widget-container").appendChild(widget);
+    // find the element in the commodity array
+    let c = commodityList.find(
+      (commodity) => commodity.id === parseInt(selectedOption)
+    );
+    tempname = c.name;
+  });
+}
+
+const makeWidget = (selectedOption) => {
+  // find the element in the commodity array
+  let c = commodityList.find(
+    (commodity) => commodity.id === parseInt(selectedOption)
+  );
+  const widgetContainer = document.createElement("div");
+  widgetContainer.classList.add("widget");
+
+  const title = document.createElement("h2");
+  title.textContent = c.name;
+
+  widgetContainer.appendChild(title);
+
+  const description = document.createElement("p");
+  description.textContent = c.information;
+  widgetContainer.appendChild(description);
+
+  return widgetContainer;
+};
