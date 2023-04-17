458d9c7bf549520083246642aa462ad24757ae3e
Fidah <69793989+fidahhha@users.noreply.github.com>
2023-04-17 14:33:32 +1200

    [Update] Converting bytes to hex
    Successfully converting bytes to hex after reading in a text file

--- COMPX301/LZdecode.java
+++ COMPX301/LZdecode.java
@@ -3,4 +3,5 @@ public class LZdecode {
     public static void main(String[] args) {
 
     }
+
 }
--- COMPX301/LZencode.java
+++ COMPX301/LZencode.java
@@ -1,6 +1,52 @@
+import java.util.*;
+
 public class LZencode {
 
-    public static void main(String[] args) {
+    /**
+     * lz78Compress
+     * Compresses a byte array using the LZ78 algorithm and returns a list of
+     * integer pairs representing the compressed data.
+     * 
+     * @param bytes
+     * @return
+     */
+    public static List<int[]> LZEncode(byte[] bytes) {
+        // Initialize Dictionary
+        Map<String, Integer> dictionary = new HashMap<>();
 
+        // Initialize variables
+        List<int[]> result = new ArrayList<>();
+        StringBuilder currentHex = new StringBuilder();
+        int counter = 1;
+
+        // Loop over bytes in the array
+        for (byte b : bytes) {
+            // Format value
+            String currentChar = String.format("%02x", b);
+            String currentString = currentHex.toString() + currentChar;
+
+            // Check if currentHex exists in the array
+            if (dictionary.containsKey(currentString)) {
+                currentHex = new StringBuilder(currentString);
+            } else {
+                // IF it doesnt exist then add it to the dictionary
+                int[] pair = { dictionary.getOrDefault(currentHex.toString(), 0), b & 0xFF };
+                result.add(pair);
+                dictionary.put(currentString, counter);
+                currentHex = new StringBuilder(currentChar);
+                // Increment the counter
+                counter++;
+            }
+        }
+
+        // Check if there are any valuse left in the currentHex
+        if (currentHex.length() > 0) {
+            // IF value does exist then add it to the output/result
+            // -1 indicates there are no values left to be encoded
+            int[] pair = { dictionary.getOrDefault(currentHex.toString(), 0), -1 };
+            result.add(pair);
+        }
+
+        return result;
     }
 }
